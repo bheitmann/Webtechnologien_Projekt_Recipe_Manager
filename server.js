086b5de237793cff73666f7850a55fef4544cf37
j-martens05 <jumahloh@gmail.com>
2026-02-22 16:19:01 +0100

    Dashboard Elemente hinzugefügt und Funktion zum Laden der Rezepte ins Dashboard implementiert.(funktioniert noch nicht ganz, da die Rezepte nicht angezeigt werden, aber die Funktion wird aufgerufen und es gibt keine Fehlermeldungen in der Konsole)

--- server.js
+++ server.js
@@ -52,3 +52,23 @@ app.post('/login', async (req, res) => {
 db.initDb().then(() => {
     app.listen(3000, () => console.log('Server running on http://localhost:3000'));
 });
+
+// Dashboard-API
+
+//Route zum Abrufen aller Rezepte eines Benutzers
+app.get('/recipes', async (req, res) => {
+    try{
+        const sql = 'SELECT * FROM recipes WHERE userId = ?'; 
+        const recipes = await dbUtils.all(sql, [req.session.userId]); // Nutzt Prepared Statements gegen SQL-Injection
+        res.json(recipes);
+    } catch (err) {
+        console.error("Fehler beim Abrufen der Rezepte:", err);
+        res.status(500).json({ success: false, message: "Interner Serverfehler." });
+    }
+});
+
+app.delete('/recipes/:id', async (req, res) => {
+    const { id } = req.params;
+    await dbUtils.run('DELETE FROM recipes WHERE id = ?', [id]);
+    res.json({ success: true });
+});
