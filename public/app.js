ca8c08d87ced5abd7ad58f554a664f0fa8be701c
bheitmann <benedikt24.heitmann@gmail.com>
2026-02-23 14:46:09 +0100

    Weitere Merge Konflikte behoben

--- public/app.js
+++ public/app.js
@@ -12,6 +12,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const show = (id) => {
             document.querySelectorAll('section').forEach(s => s.classList.add('hidden'));
             document.getElementById(id).classList.remove('hidden');
+            if(id === 'dashboard-section') {
+                loadRecipes();
+            }
         };
 
     const updateUI = () => {
